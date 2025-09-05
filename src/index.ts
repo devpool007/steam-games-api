97314daf7489b27b9e7238f1f50b39f4ecfa46fe
devpool007 <sharmadevansh007777@gmail.com>
2025-09-05 13:00:17 +0200

    corrected file names

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./api/getSteamGameDetails";
 export * from "./api/getSteamGameNamefromID";
 export * from "./api/getSteamIDforGame";
 export * from "./api/getSteamPriceOverviews";
-export * from "./api/getSteamIDsforGame";
+export * from "./api/getSteamIDsForGame";
 // Add more exports as needed for other API files
